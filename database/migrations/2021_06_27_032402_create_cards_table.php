fd2a6a241e228f35259b40fc1c5b1b23a24bb41a
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2021-06-27 08:17:58 -0600

    creando controlador, modelo y migracion de card

---
+++ database/migrations/2021_06_27_032402_create_cards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+	    Schema::create('cards', function (Blueprint $table) {
+		    $table->id();
+		    $table->string('title');
+		    $table->string('image');
+		    $table->text('description');
+		    $table->boolean('active');
+		    $table->timestamps();
+		    $table->softDeletes();
+	    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
